aa56abada5a7fc3cbfcbf49a270b17cc1671d4a8
Sergei Kolyshkin <kolyshkin@selectel.ru>
2021-09-07 17:33:28 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
+const helloMsg = "Hello, OTUS!"
+
 func main() {
-	// Place your code here.
+	fmt.Println(stringutil.Reverse(helloMsg))
 }
